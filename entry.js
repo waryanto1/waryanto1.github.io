20ec42f8fc0a2d04ff3f1641a9c4252ee8dea4a6
waryanto1 <thefuturetech.com@gmail.com>
2020-02-01 08:53:07 +0700

    Update entry.js

--- entry.js
+++ entry.js
@@ -7,13 +7,13 @@ document.write(`
   integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
   crossorigin="anonymous"
 />
-<link rel="stylesheet" href="https://raw.githubusercontent.com/waryanto1/chordlagu/master/style.css" />
+<link rel="stylesheet" href="https://waryanto1.github.io/style.css" />
 <div id="app" class="container">
   <h2>Loading dulu...</h2>
 </div>
-<script defer src="https://raw.githubusercontent.com/waryanto1/chordlagu/master/sharp11-harmony.js?v=1564040417552"></script>
-<script defer src="https://raw.githubusercontent.com/waryanto1/chordlagu/master/vue.js"></script>
-<script defer src="https://raw.githubusercontent.com/waryanto1/chordlagu/master/vue-youtube-embed.umd.js"></script>
-<script defer src="https://raw.githubusercontent.com/waryanto1/chordlagu/master/index.js"></script>
-<script defer src="https://raw.githubusercontent.com/waryanto1/chordlagu/master/script.js"></script>
+<script defer src="https://waryanto1.github.io/sharp11-harmony.js?v=1564040417552"></script>
+<script defer src="https://waryanto1.github.io/vue.js"></script>
+<script defer src="https://waryanto1.github.io/vue-youtube-embed.umd.js"></script>
+<script defer src="https://waryanto1.github.io/index.js"></script>
+<script defer src="https://waryanto1.github.io/script.js"></script>
 `)
