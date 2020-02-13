82df3de931f2c9cca57e15b20498f1c8a096eb5c
waryanto1 <thefuturetech.com@gmail.com>
2020-02-13 11:30:11 +0700

    Update sfl.js

--- sfl.js
+++ sfl.js
@@ -129,7 +129,7 @@ j++;
 }
 if (a_to_vh == false) {
 var encryptedUrl = Base64.encode(a_to_vi);
-a_to_ve[i].href = "https://handypedia.com/hello-world?url=" + encryptedUrl;
+a_to_ve[i].href = "https://handypedia.com/safelink?url=" + encryptedUrl;
 a_to_ve[i].rel = "nofollow";
 a_to_vb++;
 a_to_vc += i + ":::" + a_to_ve[i].href + "\n";
